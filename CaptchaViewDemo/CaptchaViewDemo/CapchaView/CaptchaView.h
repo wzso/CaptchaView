dc4dea4d7fc9d5e50c12beaa4d651c15876ae450
wenzhishen <wenzhishen@icloud.com>
2015-11-27 23:22:53 +0800

    Start of this little project

---
+++ CaptchaViewDemo/CaptchaViewDemo/CapchaView/CaptchaView.h
@@ -0,0 +1,13 @@
+//
+//  CaptchaView.h
+//  CaptchaViewDemo
+//
+//  Created by Vincent on 27/11/2015.
+//  Copyright © 2015 wenzhishen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaptchaView : UIView
+
+@end
---
+++ CaptchaViewDemo/CaptchaViewDemo/CapchaView/CaptchaView.m
@@ -0,0 +1,14 @@
+//
+//  CaptchaView.m
+//  CaptchaViewDemo
+//
+//  Created by Vincent on 27/11/2015.
+//  Copyright © 2015 wenzhishen.com. All rights reserved.
+//
+
+#import "CaptchaView.h"
+
+@implementation CaptchaView
+
+
+@end
