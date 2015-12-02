a551cdb75e38d1838f3312048e9d661750b64428
wenzhishen <wenzhishen@icloud.com>
2015-12-03 00:12:21 +0800

    Abstract some code to methods

--- CaptchaViewDemo/CaptchaViewDemo/VFCapchaView/VFCaptchaView.m
+++ CaptchaViewDemo/CaptchaViewDemo/VFCapchaView/VFCaptchaView.m
@@ -8,11 +8,11 @@
 
 #import "VFCaptchaView.h"
 
-#define RANDOM_COLOR [UIColor colorWithRed:arc4random_uniform(1000) / 1000.f green:arc4random_uniform(1000) / 1000.f blue:arc4random_uniform(1000) / 1000.f alpha:1.f]
+#define RANDOM_COLOR [UIColor colorWithRed:arc4random_uniform(100) / 100.f green:arc4random_uniform(100) / 100.f blue:arc4random_uniform(100) / 100.f alpha:1.f]
 #define RANDOM_FONT_SIZE (arc4random_uniform(6) + 15)
 
 @interface VFCaptchaView ()
-@property (nonatomic, strong) NSArray *captchaCode;
+@property (nonatomic, strong) NSArray *captchaCharacters;
 @end
 
 @implementation VFCaptchaView
@@ -22,56 +22,91 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.layer.cornerRadius = 6.f;
-        self.captchaCode = [self randomCaptchaCode];
+        self.captchaCharacters = [self randomCaptchaCharacters];
     }
     
     return self;
 }
 
 - (void)setCaptchaCode:(NSString *)captchaCode {
-    _captchaCode = [captchaCode componentsSeparatedByString:@""];
-    self.codeLength = _captchaCode.count;
+    self.captchaCharacters = [self charactersInString:captchaCode];
+    self.codeLength = self.captchaCharacters.count;
     [self setNeedsDisplay];
 }
 
 - (void)randomlySetCaptchaCode {
-    self.captchaCode = [self randomCaptchaCode];
+    self.captchaCharacters = [self randomCaptchaCharacters];
+    [self setNeedsDisplay];
 }
 
 #pragma mark - Private Methods
-- (NSString *)randomCaptchaCode {
-    NSMutableString *captchaCode = [NSMutableString string];
-    NSArray *array = [@"阿里巴巴和四十万大盗开业庆典abcdefghijklmnopqrstuvwxyz这是一个简单的验证码视图ABCDEFGHIJKLMNOPQRSTUVWXYZ你通过手动设置验证码0123456789随机生成的也行" componentsSeparatedByString:@""];
+- (NSArray *)randomCaptchaCharacters {
+    NSMutableArray *captchaCharacters = [NSMutableArray array];
+    NSArray *array = [self charactersInString:@"阿里巴巴和四十万大盗开业庆典abcdefghijklmnopqrstuvwxyz这是一个简单的验证码视图ABCDEFGHIJKLMNOPQRSTUVWXYZ你通过手动设置验证码0123456789随机生成的也行"];
     if (!self.codeLength) {
         self.codeLength = 6;
     }
+    u_int32_t count = (u_int32_t)array.count;
     for (int i = 0; i < self.codeLength; i ++) {
-        NSString *character = array[arc4random_uniform((unsigned int)array.count)];
-        [captchaCode appendString:character];
+        NSString *character = array[arc4random_uniform(count)];
+        NSLog(@"%@", character);
+        [captchaCharacters addObject:character];
     }
     
-    return captchaCode;
+    return captchaCharacters;
+}
+
+- (CGRect)rectForCharacterInFont:(UIFont *)font atIndex:(NSUInteger)index {
+    CGFloat margin = 2.f;
+    // max width for each character
+    CGFloat maxWidth = (self.frame.size.width - (self.captchaCharacters.count + 1) * margin) / self.captchaCharacters.count;
+    // max height of a character
+    CGFloat maxHeight = self.frame.size.height;
+    
+    // the size of the rectangle a character can draw in
+    CGSize size = [@"哈" boundingRectWithSize:CGSizeMake(maxWidth, maxHeight) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:font} context:nil].size;
+    CGFloat width = size.width;
+    CGFloat height = size.height;
+    u_int32_t widthDiff = (u_int32_t)(maxWidth - width);
+    u_int32_t heightDiff = (u_int32_t)(maxHeight - height);
+    
+    return CGRectMake(index * width + arc4random_uniform(widthDiff), arc4random_uniform(heightDiff), width, height);
+}
+
+- (NSArray *)charactersInString:(NSString *)string {
+    NSMutableArray *characters = [NSMutableArray array];
+    for (int i = 0; i < string.length; i ++) {
+        [characters addObject:[NSString stringWithFormat:@"%c",[string characterAtIndex:i]]];
+    }
+    return characters;
 }
 
 #pragma mark - Override Methods
 - (void)drawRect:(CGRect)rect {
     self.backgroundColor = RANDOM_COLOR;
     
-    CGFloat margin = 2.f;
-    // max width of a character
-    CGFloat width = (self.frame.size.width - (self.captchaCode.count + 1) * margin) / self.captchaCode.count;
-    // max height of a character
-    CGFloat height = [@"哈" boundingRectWithSize:CGSizeMake(width, self.frame.size.height) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20]} context:nil].size.height;
-    
     __weak typeof(self) weakSelf = self;
     NSArray *familyNames = [UIFont familyNames];
     u_int32_t namesCount = (u_int32_t)familyNames.count;
-    [self.captchaCode enumerateObjectsUsingBlock:^(NSString *  _Nonnull character, NSUInteger idx, BOOL * _Nonnull stop) {
-        
-        
+    
+    // draw characters
+    [self.captchaCharacters enumerateObjectsUsingBlock:^(NSString *  _Nonnull character, NSUInteger idx, BOOL * _Nonnull stop) {
+        CGFloat fontSize = RANDOM_FONT_SIZE;
         NSString *fontName = familyNames[arc4random_uniform(namesCount)];
-        [character drawInRect:CGRectMake(0, 0, 0, 0) withAttributes:@{NSFontAttributeName:[UIFont fontWithName:fontName size:RANDOM_FONT_SIZE], NSForegroundColorAttributeName:RANDOM_COLOR}];
+        // the font to draw in.
+        UIFont *font = [UIFont fontWithName:fontName size:fontSize];
+        
+        [character drawInRect:[weakSelf rectForCharacterInFont:font atIndex:idx] withAttributes:@{NSFontAttributeName:font, NSForegroundColorAttributeName:RANDOM_COLOR}];
     }];
+    
+    // draw line for disturbance
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    self.captchaCharacters = [self randomCaptchaCharacters];
+    [self setNeedsDisplay];
 }
 
 @end
--- CaptchaViewDemo/CaptchaViewDemo/ViewController.m
+++ CaptchaViewDemo/CaptchaViewDemo/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "VFCaptchaView.h"
+
+#define SCREEN_WIDTH self.view.frame.size.width
+#define SCREEN_HEIGHT self.view.frame.size.height
 
 @interface ViewController ()
 
@@ -17,6 +21,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    VFCaptchaView *captchaView = [[VFCaptchaView alloc] initWithFrame:CGRectMake(20.f, 40.f, SCREEN_WIDTH - 40.f, 40) success:^(NSString *verificationCode) {
+        NSLog(@"Wrong Code: %@. Go!", verificationCode);
+    } failure:^{
+        NSLog(@"Right Code, Pity!");
+    }];
+    [captchaView setCaptchaCode:@"你是一只老乌龟"];
+    captchaView.codeLength = 10;
+    [self.view addSubview:captchaView];
 }
 
 @end
